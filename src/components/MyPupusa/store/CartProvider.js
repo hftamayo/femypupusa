ad096345de17c56cafecdac3949b68d82b674e4a
hftamayo <hftamayo@gmail.com>
2022-04-11 22:20:59 -0600

    working on store components part2

--- src/components/MyPupusa/store/CartProvider.js
+++ src/components/MyPupusa/store/CartProvider.js
@@ -41,22 +41,57 @@ const cartReducer = (state, action) => {
     const existingItem = state.items[existingCartItemIndex];
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedITems;
-    if(existingItem.amount === 1){
-        updatedItems = state.items.filter(item => item.id !== action.id);
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
-        const updatedItem = { ...existingItem, amount: existingItem.amount -1 };
-        updatedItems = [...state.items];
-        updatedItems[existingCartItemIndex] = updatedItem;
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
     }
-    return{
-        items: updatedItems,
-        totalAmount: updatedTotalAmount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
 
-  if(action.type === 'CLEAR'){
-      return defaultCartState;
+  if (action.type === "CLEAR") {
+    return defaultCartState;
   }
 
   return defaultCartState;
 };
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
+  const CartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
